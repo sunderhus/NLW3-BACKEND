41fa532b179226861761c4a7b9797ddb7bfe1a90
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-11-09 18:27:53 -0300

    fix: folder controller name pattern

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "./config/upload";
-import OrphanagesController from "./controllers/OrphanagesController";
+import OrphanagesController from "./constrollers/OrphanagesController";
 
 const routes = Router();
 const upload = multer(uploadConfig);
