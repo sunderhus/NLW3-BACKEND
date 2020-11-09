6c9747910731d8beaf03d425ff29a3498daa12e1
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-11-09 17:58:46 -0300

    fix: folder nomenclature

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "./config/upload";
-import OrphanagesController from "./Controllers/OrphanagesController";
+import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
 const upload = multer(uploadConfig);
